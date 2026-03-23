231c423bc95a19a6e283e0b9dbd6d5b84033709c
PedObra <pedobra@pedobra.app>
2026-03-23 19:40:25 -0300

    fix(dashboard): use valid postgres type for cache buster in order fetching

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -19,7 +19,7 @@ const AdminDashboard = () => {
             const { data, error } = await supabase
                 .from('orders')
                 .select('*, sites(name), profiles(name)')
-                .not('id', 'eq', `bust-${Date.now()}`) // Cache-buster dinâmico para evitar Cache 200 (Disk) do navegador
+                .lte('created_at', new Date().toISOString()) // Cache-buster dinâmico que o Postgre entende perfeitamente
                 .order('created_at', { ascending: false });
 
             if (error) throw error;
